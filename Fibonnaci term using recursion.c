b3f203e800fef0b6298fc48fd40a92cc8109fd13
Pukar Rimal <110043874+Pukar77@users.noreply.github.com>
2023-07-02 19:52:31 +0545

    Add files via upload

---
+++ Fibonnaci term using recursion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int fib(int n);
+
+
+int main(){
+	int n;
+	
+	printf("Enter the term of which you want fibonnaci number\n");
+	scanf("%d",&n);
+	printf("Fibonnaci number is %d", fib(n));
+	
+}
+int fib(int n){
+	if(n<=1){
+		return 1;
+	}
+	else{
+		return fib(n-1)+fib(n-2);
+	}
+}

89dd719e88216cdb6d6218cbd48f130d1fbdb5ea
Pukar Rimal <110043874+Pukar77@users.noreply.github.com>
2023-07-19 19:14:05 +0545

    Add files via upload

---
+++ Linked list/Inserting newnode at begining of doubly linked list.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct list{
+	int data;
+	struct list *next;
+	struct list *prev;
+};
+
+typedef struct list node;
+
+int main(){
+	node *first= (node*)malloc(sizeof(node));
+	node *second= (node*)malloc(sizeof(node));
+	node *third= (node*)malloc(sizeof(node));
+	
+	first->data=10;
+	second->data=20;
+	third->data=30;
+	
+	first->next=second;
+	second->next=third;
+	third->next=NULL;
+	first->prev=NULL;
+	second->prev=first;
+	third->prev=second;
+	
+	node* newnode=(node*)malloc(sizeof(node));
+	newnode->data=40;
+	
+	node *ptr=first;
+	
+	newnode->next=first;
+	first->prev=newnode;
+	first=newnode;
+	
+	ptr=first;
+	while(ptr!=NULL){
+		printf("%d\t",ptr->data);
+		ptr=ptr->next;
+	}
+	
+	
+	
+}
